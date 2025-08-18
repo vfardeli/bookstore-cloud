17afc5bc2b617fa246f990ea6c61797aa0d9c2f8
vfardeli <valeryfardeli@gmail.com>
2025-08-18 14:46:15 -0700

    feat: add API gateway and refactor

--- order-service/internal/models/order.go
+++ order-service/internal/models/order.go
@@ -1,9 +1,17 @@
 package models
 
 type Order struct {
-	ID       uint `gorm:"primaryKey"`
-	UserID   uint
-	BookID   uint
-	Quantity int
-	Status   string
+	ID       uint   `json:"id" gorm:"primaryKey"`
+	UserID   uint   `json:"user_id"`
+	BookID   uint   `json:"book_id"`
+	Quantity int    `json:"quantity"`
+	Status   string `json:"status"`
+}
+
+type Book struct {
+	ID     uint    `json:"id"`
+	ISBN   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author string  `json:"author"`
+	Price  float64 `json:"price"`
 }
--- payment-service/internal/models/payment.go
+++ payment-service/internal/models/payment.go
@@ -1,7 +1,7 @@
 package models
 
 type Payment struct {
-	OrderID uint    `json:"orderId"`
+	OrderID uint    `json:"order_id"`
 	Amount  float64 `json:"amount"`
 	Method  string  `json:"method"`
 	Status  string  `json:"status"`
